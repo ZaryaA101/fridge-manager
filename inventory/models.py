740e5d1b94f947e4f3838c181596ca3f90645dd8
evannpham <enavpham@gmail.com>
2025-04-05 22:55:15 +0000

    Added Item model and basic inventory page

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,16 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    exp_date = models.DateField("Expiration date")
+    description = models.CharField(max_length=200)
+
+    def will_expire_soon(self):
+        return self.exp_date >= timezone.now() - datetime.timedelta(days=4)
+
+    def __str__(self):
+        return self.name
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Item
+
 # Create your views here.
+def index(request):
+    item_list = Item.objects.all()
+    context = {
+        "item_list": item_list,
+    }
+    return render(request, "inventory/index.html", context)
