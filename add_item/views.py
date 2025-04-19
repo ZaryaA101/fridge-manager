797ddf6bd8671c023e54be68e2e18f7085993deb
ZaryaA101 <zarya.amin01@student.csulb.edu>
2025-04-18 19:48:16 -0700

    Update views.py

--- add_item/views.py
+++ add_item/views.py
@@ -4,13 +4,13 @@ from django.contrib import messages
 from .models import FridgeContent, FridgeDetail
 
 def add_item(request):
-    family = request.user.family_set.first()  # Get the family of the logged-in user
-    fridge = FridgeDetail.objects.get(family_id=family)  # Get the fridge for the family
-    total_items = FridgeContent.objects.filter(family_id=family).count()  # Total number of items
+    family = request.user.family_set.first() 
+    fridge = FridgeDetail.objects.get(family_id=family)  
+    total_items = FridgeContent.objects.filter(family_id=family).count() 
 
-    if total_items >= fridge.capacity:  # Check if fridge is full
+    if total_items >= fridge.capacity:  
         messages.error(request, "Your fridge is full. Please remove an item before adding more.")
-        return redirect('home')  # Redirect to home or any other page
+        return redirect('home')  
 
     if request.method == "POST":
         form = ItemForm(request.POST, request.FILES)
@@ -18,7 +18,7 @@ def add_item(request):
             new_item = form.save(commit=False)
             new_item.family_id = family
             new_item.save()
-            # Update fridge content count or other actions as needed
+           
             return redirect('home')
     else:
         form = ItemForm()
