60962b69dc24994ccfe2c807ff1efa0ddb08815b
ZaryaA101 <zarya.amin01@student.csulb.edu>
2025-04-18 19:56:04 -0700

    Update settings.py to Use Celery with Redis for Notifications

--- fridge_manager/settings.py
+++ fridge_manager/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +129,7 @@ MEDIA_ROOT = 'fridge_manager/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
