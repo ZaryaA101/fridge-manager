5df89b0e6075cf8069abfc21fe4e7b5bead8cf49
ZaryaA101 <zarya.amin01@student.csulb.edu>
2025-04-20 01:51:55 -0700

    Update scheduler.py

--- inventory/scheduler.py
+++ inventory/scheduler.py
@@ -1,1 +1,8 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from inventory.tasks import check_item_expiration  
+
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(check_item_expiration, 'interval', hours=24)
+    scheduler.start()
 
