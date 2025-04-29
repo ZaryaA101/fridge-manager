aca39e45c91ee93a5db63fa0fbd7092de3722355
AnthonyC221 <cancholaanthony64@gmail.com>
2025-04-28 23:36:43 -0700

    Update urls.py
    updated and added a couple of urls for update item remove item remove fridge item and unregister item

--- add_item/urls.py
+++ add_item/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('additem', views.add_item, name='add_item'),
     path('update-item/<uuid:item_id>/', views.update_item, name='update_item'),
+    path('remove-item/<uuid:item_id>/', views.remove_item, name='remove_item'),
+    path('remove-fridge-item/<uuid:item_id>/', views.remove_fridge_item, name='remove_fridge_item'),
+
+    path('unregister-item/', views.unregister_item_page, name='unregister_item_page'),
+    path('unregister-item/submit/', views.unregister_item, name='unregister_item'),
 ]
