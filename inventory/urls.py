dab41cdc9519054dde476c81523c7a24008b7898
AnthonyC221 <cancholaanthony64@gmail.com>
2025-04-28 23:38:59 -0700

    Update urls.py
    added from import that imported the views from the add_item folder and added urls for each of them

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from add_item.views import update_item, remove_item, remove_fridge_item, unregister_item, unregister_item_page
 
 from . import views
 
@@ -16,4 +17,11 @@ urlpatterns = [
     path('my_families/', views.my_families, name='my_families'),
     path('family/<uuid:family_id>/manage_fridge/', views.manage_fridge_details, name='manage_fridge_details'),
     path('family/<uuid:family_id>/add_fridge_item/', views.add_fridge_item, name='add_fridge_item'),
+
+    path('update-item/<str:item_id>/', update_item, name='update_item'),
+    path('remove-item/<str:item_id>/', remove_item, name='remove_item'),
+    path('remove-fridge-item/<str:item_id>/', remove_fridge_item, name='remove_fridge_item'),
+
+    path('unregister-item/', unregister_item_page, name='unregister_item_page'),
+    path('unregister-item/submit/', unregister_item, name='unregister_item'),
 ]
