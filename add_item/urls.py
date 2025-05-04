08ee197600b0eedb3e2e0dd8420add907f21995e
AnthonyC221 <cancholaanthony64@gmail.com>
2025-05-04 00:57:59 -0700

    Update urls.py
    paths updated to include add item, remove item, and update item

--- add_item/urls.py
+++ add_item/urls.py
@@ -2,11 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('additem', views.add_item, name='add_item'),
-    path('update-item/<uuid:item_id>/', views.update_item, name='update_item'),
-    path('remove-item/<uuid:item_id>/', views.remove_item, name='remove_item'),
-    path('remove-fridge-item/<uuid:item_id>/', views.remove_fridge_item, name='remove_fridge_item'),
-
-    path('unregister-item/', views.unregister_item_page, name='unregister_item_page'),
-    path('unregister-item/submit/', views.unregister_item, name='unregister_item'),
+    path('family/<uuid:family_id>/add_item/', views.add_item, name='add_item'),
+    path('remove-item/<str:item_id>/', views.remove_item, name='remove_item'),
+    path('update-item/<str:item_id>/', views.update_item, name='update_item'),  
 ]
