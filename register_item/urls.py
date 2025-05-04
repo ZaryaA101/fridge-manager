b3a9b8023d973a223bc165fb54c5287e5e7646cc
AnthonyC221 <cancholaanthony64@gmail.com>
2025-05-04 01:12:43 -0700

    Update urls.py
    added register paths

--- register_item/urls.py
+++ register_item/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register-item', views.register_item, name='register_item'),
-    #path('update-item/<str:item_id>/', views.update_item, name='update_item'),
-    #path('remove-item/<str:item_id>/', views.remove_item, name='remove_item'),
-    #path('remove-fridge-item/<str:item_id>/', views.remove_fridge_item, name='remove_fridge_item'),
     path('unregister-item/', views.unregister_item_page, name='unregister_item_page'),
     path('unregister-item/submit/', views.unregister_item, name='unregister_item'),
 ]
